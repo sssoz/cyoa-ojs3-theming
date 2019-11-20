7bff79584782e350931d70114b6884cf28bd18dc
Sophy <sssoz@users.noreply.github.com>
2019-11-20 16:35:12 +0100

    Fix indentation

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
-var start      			  	 	= document.getElementById('start'),
-		colours								= document.getElementById('colours'),
-		coloursLow						= document.getElementById('colours-low'),
-		coloursMedium					= document.getElementById('colours-medium'),
-		coloursHigh						= document.getElementById('colours-high'),
-		coloursHighMore				= document.getElementById('colours-high-more'),
+var start      			= document.getElementById('start'),
+    colours			= document.getElementById('colours'),
+    coloursLow			= document.getElementById('colours-low'),
+    coloursMedium		= document.getElementById('colours-medium'),
+    coloursHigh			= document.getElementById('colours-high'),
+    coloursHighMore		= document.getElementById('colours-high-more'),
 
-		typography						= document.getElementById('typography'),
-		typographyLow					= document.getElementById('typography-low'),
-		typographyMedium			= document.getElementById('typography-medium'),
-		typographyMediumMore	= document.getElementById('typography-medium-more'),
-		typographyHigh				= document.getElementById('typography-high'),
-		typographyHighMore		= document.getElementById('typography-high-more'),
+    typography			= document.getElementById('typography'),
+    typographyLow		= document.getElementById('typography-low'),
+    typographyMedium		= document.getElementById('typography-medium'),
+    typographyMediumMore	= document.getElementById('typography-medium-more'),
+    typographyHigh		= document.getElementById('typography-high'),
+    typographyHighMore		= document.getElementById('typography-high-more'),
 
-		logo									= document.getElementById('logo'),
-		logoLow								= document.getElementById('logo-low'),
-		logoMedium						= document.getElementById('logo-medium'),
-		logoMediumMore				= document.getElementById('logo-medium-more'),
-		logoHigh							= document.getElementById('logo-high'),
-		logoHighMore					= document.getElementById('logo-high-more'),
+    logo			= document.getElementById('logo'),
+    logoLow			= document.getElementById('logo-low'),
+    logoMedium			= document.getElementById('logo-medium'),
+    logoMediumMore		= document.getElementById('logo-medium-more'),
+    logoHigh			= document.getElementById('logo-high'),
+    logoHighMore		= document.getElementById('logo-high-more'),
 
-		visuals								= document.getElementById('visuals'),
-		visualsLow						= document.getElementById('visuals-low'),
-		visualsMedium					= document.getElementById('visuals-medium'),
-		visualsMediumMore			= document.getElementById('visuals-medium-more'),
+    visuals			= document.getElementById('visuals'),
+    visualsLow			= document.getElementById('visuals-low'),
+    visualsMedium		= document.getElementById('visuals-medium'),
+    visualsMediumMore		= document.getElementById('visuals-medium-more'),
 
-		theme									= document.getElementById('theme'),
-		themeManuscript				= document.getElementById('theme-manuscript'),
-		themeHealthSciences		= document.getElementById('theme-health-sciences'),
-		themeClassic					= document.getElementById('theme-classic'),
-		themeImmersion				= document.getElementById('theme-immersion'),
-		themePragma						= document.getElementById('theme-pragma');
+    theme			= document.getElementById('theme'),
+    themeManuscript		= document.getElementById('theme-manuscript'),
+    themeHealthSciences		= document.getElementById('theme-health-sciences'),
+    themeClassic		= document.getElementById('theme-classic'),
+    themeImmersion		= document.getElementById('theme-immersion'),
+    themePragma			= document.getElementById('theme-pragma');
 
 function showStep(step) {
-	var activeElem			 		= event.target;
+	var activeElem		= event.target;
 
 	step.classList.toggle("open");
 
